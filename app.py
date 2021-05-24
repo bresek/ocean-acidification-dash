5ad0b35cb64f6f6e111b1c8e702c551aca009136
Ben Resek <bresek@gmail.com>
2021-05-23 21:33:56 -0400

    fixing variable names

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+# Talked with Prof. Magee and we did not see a purpose in having an __init__ or __repr__ method at the present. She told me to write this here
+#! Run this app with `python app.py` in your terminal and
+#! visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from dash.dependencies import Input, Output
 import pandas as pd
 import os
 import matplotlib
 
 
 
-# defining data processing functions 
+# defining data processing functions
 class dataCleaning:
     def folderToDfs(folderpath):
         """returns list of dataframes from an input folder"""
@@ -20,6 +22,7 @@ class dataCleaning:
         df_list = []
         for file in files:
             if not file.startswith('.'):
+                print(file)
                 temp_df = pd.read_csv(folderpath+'/'+file)
                 df_list.append(temp_df)
         return df_list
@@ -38,7 +41,7 @@ class dataCleaning:
 
     def cleanedDailyAvg(dataFrame):
         """Takes a data frame. Averages variables by day"""
-        temp = dataFrame[dataFrame['pH_total_scale'] > 0]
+        temp = dataFrame[dataFrame['pH_total_scale'] != -999]
         return temp.groupby(['Date']).mean()
 
 
@@ -47,39 +50,125 @@ ME_dfs = dataCleaning.folderToDfs('analysis/data/ME')
 GA_dfs = dataCleaning.folderToDfs('analysis/data/GA')
 FL_dfs = dataCleaning.folderToDfs('analysis/data/FL')
 
-## Maine
+# Maine
 
-columns = {
+
+ME_cols =  {
+    "pH_SW": "pH_total_scale",
+    "pH_SW (Total Scale)": "pH_total_scale",
+    "pH (total scale)": "pH_total_scale",
+    'pH_QF' : 'pH QF',
+    'CHL': "CHL (ug/l)",
+    'xCO2  Air (wet) (umol/mol)': 'xCO2 Air (wet) (umol/mol)',
+    'xCO2  Air (wet) (umol/mol)': 'xCO2 Air (wet) (umol/mol)',
+    'DOXY': 'DOXY (umol/kg)',
+    'Licor Atm Pressure  (hPa)': 'Licor Atm Pressure (hPa)',
+    'NTU': 'NTU (NTU)',
+    'fCO2  Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
+    'fCO2  SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
+    'fCO2 Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
+    'pCO2 Air (sat) uatm': 'pCO2 Air (sat) (uatm)',
+    'fCO2 SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
+    'fCO2  SW (sat) (uatm)': 'fCO2 SW (sat) (uatm)',
+    'pCO2 SW (sat) uatm': 'pCO2 SW (sat) (uatm)',
+    'xCO2  SW (dry) (umol/mol)': 'xCO2 SW (dry) (umol/mol)',
+    'xCO2  SW (wet) (umol/mol)': 'xCO2 SW (wet) (umol/mol)',
+    'xCO2  Air (dry) (umol/mol)': 'xCO2 Air (dry) (umol/mol)'
+
+}
+FL_cols = {
     "pH_SW": "pH_total_scale",
     "pH_SW (Total Scale)": "pH_total_scale",
     "pH (total scale)": "pH_total_scale",
     "pH SW": "pH_total_scale",
-    "pH_Total_Scale": "pH_total_scale"
+    "pH_Total_Scale": "pH_total_scale",
+        'CHL': "CHL (ug/l)",
+        'xCO2 Air (wet) (umol/mol)': 'xCO2 Air (wet) (umol/mol)',
+
+        'Licor Atm Pressure  (hPa)': 'Licor Atm Pressure (hPa)',
+        'fCO2  Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
+        'fCO2  SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
+        # 'fCO2 Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
+        'pCO2 Air (sat) uatm': 'pCO2 Air (sat) (uatm)',
+        # 'fCO2 SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
+        # 'fCO2  SW (sat) (uatm)': 'fCO2 SW (sat) (uatm)',
+        'pCO2 SW (sat) uatm': 'pCO2 SW (sat) (uatm)',
+        "% O2": "MAPCO2 %O2",
+        'DOXY': 'DOXY (umol/kg)',
+        'NTU' : 'NTU (NTU)',
+        
+        'xCO2  Air (wet) (umol/mol)': 'xCO2 Air (wet) (umol/mol)',
+        'xCO2  SW (dry) (umol/mol)': 'xCO2 SW (dry) (umol/mol)',
+        'xCO2  Air (dry) (umol/mol)': 'xCO2 Air (dry) (umol/mol)',
+        'xCO2  SW (wet) (umol/mol)': 'xCO2 SW (wet) (umol/mol)',
+        'fCO2 Air (sat) uatm' : 'fCO2 Air (sat) (uatm)',
+        'fCO2 SW (sat) uatm' : 'fCO2 SW (sat) (uatm)'
+
+
+
 }
-dataCleaning.renameCols(ME_dfs, columns)
+
+
+GA_cols = {
+    "pH_SW": "pH_total_scale",
+    "pH_SW (Total Scale)": "pH_total_scale",
+    "pH (total scale)": "pH_total_scale",
+    "pH SW": "pH_total_scale",
+    "pH_Total_Scale": "pH_total_scale",
+    # 'fCO2 Air (sat) uatm': 'fCO2 Air (sat) (uatm)'
+
+}
+
+dataCleaning.renameCols(ME_dfs, ME_cols)
 ME_DF = dataCleaning.combineVertically(ME_dfs)
 ME_DF_AVG = dataCleaning.cleanedDailyAvg(ME_DF)
 
 
-## Georgia
+# Georgia
 
-dataCleaning.renameCols(GA_dfs, columns)
+dataCleaning.renameCols(GA_dfs, GA_cols)
 GA_DF = dataCleaning.combineVertically(GA_dfs)
 GA_DF_AVG = dataCleaning.cleanedDailyAvg(GA_DF)
 
 
-## Florida
-dataCleaning.renameCols(FL_dfs, columns)
+# Florida
+dataCleaning.renameCols(FL_dfs, FL_cols)
 FL_DF = dataCleaning.combineVertically(FL_dfs)
 FL_DF_AVG = dataCleaning.cleanedDailyAvg(FL_DF)
 
 
-# combine into one 
+# add site variable for single data frame
 ME_DF_AVG['site'] = 'ME'
 GA_DF_AVG['site'] = 'GA'
 FL_DF_AVG['site'] = 'FL'
 
+# create one single data frame
 all_df_avg = pd.concat([ME_DF_AVG, GA_DF_AVG, FL_DF_AVG], axis=0)
+
+# all_df_avg = all_df_avg[all_df_avg['xCO2 Air (wet) (umol/mol)'] > 300]
+
+#select varaibles to preserve
+# all_df_avg = all_df_avg[]
+
+# all_df_avg = all_df_avg[all_df_avg['CO2 SW QF'] == 2]
+
+# get list of variables for dropdown menu                         
+variables = all_df_avg.columns.sort_values()
+variables_2_use = [ 'DOXY (umol/kg)',
+                    'H2O Air (mmol/mol)',
+                   'H2O SW (mmol/mol)', 'Latitude', 'Licor Atm Pressure (hPa)',
+                   'Licor Temp (C)', 'Longitude', 'MAPCO2 %O2', 'NTU (NTU)', 'NTU QF',
+                   'PH_QF', 'QF', 'QF.1', 'QF_pH', 'SST (C)', 'Salinity', 'dfCO2', 'dpCO2',
+                   'fCO2 Air (sat) (uatm)', 'fCO2 SW (sat) (uatm)',
+                   'pCO2 Air (sat) (uatm)', 'pCO2 SW (sat) (uatm)',
+                   'pH_total_scale', 'site', 'xCO2 Air (dry) (umol/mol)',
+                   'xCO2 Air (wet) (umol/mol)', 'xCO2 SW (dry) (umol/mol)',
+                   'xCO2 SW (wet) (umol/mol)']
+# get sites for site filter drop down
+sites = all_df_avg['site'].unique()
+sites
+print(variables.sort_values())
+
 # all_df_avg[all_df_avg[]]
 # temp = dataFrame[dataFrame['SST (C)'] > 0]
 
@@ -95,38 +184,82 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # print(ME_DF_AVG['site'])
 
-graph_me = px.scatter(all_df_avg, y="pH_total_scale", trendline="ols", color='site')
+graph_me = px.scatter(all_df_avg, y="pH_total_scale",
+                      trendline="ols", color='site')
 graph_2 = px.scatter(all_df_avg[all_df_avg['SST (C)'] > 0], y="pH_total_scale", x="SST (C)",
                      trendline="ols", color='site')
 graph_fl = px.scatter(FL_DF_AVG, y="pH_total_scale", trendline="ols")
 
 
-#3 input parameters I think (1) Y variable (2) x variable (3)site (4??) avg by day, month, year?
+# 3 input parameters I think (1) Y variable (2) x variable (3)site (4??) avg by day, month, year?
+
+# Getting date to be apart of the xvariables
+xVariables = [{'label': i, 'value': i}
+     for i in variables_2_use]
+dateLabel = {'label': "Date", 'value': ''}
+xVariables.append(dateLabel)
 
 
 app.layout = html.Div(children=[
+
     html.H1(children='Ocean Acidification'),
+    html.P(children=f"Number of varibles: {len(variables_2_use)}. DataFrame dimensions: {all_df_avg.shape}"),
 
-    html.Div(children='''
-        Maine, Georgia, Florida
-    '''),
-    dcc.Graph(
-        id='me ph graph',
-        figure=graph_me
-    ),
-    dcc.Graph(
-        id="ga ph graph",
-        figure=graph_2
-    ),
-    dcc.Graph(
-        id='fl ph graph',
-        figure=graph_fl
-    ),
+    html.Div(id = "interactive plot",children =[ 
+        dcc.Dropdown(
+                id='xaxis',
+            options=xVariables,
+                value=''
+                ),
+        dcc.Dropdown(
+            id='yaxis',
+            options=[{'label': i, 'value': i} for i in variables_2_use],
+            value="pH_total_scale"
+        ),
+        
+        dcc.Dropdown(
+            id='site_selector',
+            options=[{'label': i, 'value': i} for i in sites],
+            
+            value=sites,
+            multi=True
 
 
-
+        ),
+        dcc.Graph(id='indicator-graphic'),
+   ])
 
 ])
+
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis', 'value'),
+    Input('yaxis', 'value'),
+    Input('site_selector', "value")
+)
+def update_graph(xaxis, yaxis, site_selector):
+    print(f"site_selector {site_selector}")
+    print(type(site_selector))
+
+    if type(site_selector) == list:
+        data = all_df_avg[all_df_avg.site.isin(
+            site_selector)]
+    else:
+        data = all_df_avg[all_df_avg['site'] == site_selector]
+
+
+    if xaxis == '':
+        fig = px.scatter(data, y=data[yaxis], trendline="ols", color='site')
+    else:
+        fig = px.scatter(data , x=data[xaxis],
+                         y=data[yaxis], trendline="ols", color='site')
+
+
+    fig.update_xaxes(rangeslider_visible=True)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
