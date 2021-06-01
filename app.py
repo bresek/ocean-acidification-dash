075ee6b14b54ffa0cfda4757fd96a3bb3b3f68bb
Ben Resek <bresek@gmail.com>
2021-06-01 19:00:14 -0400

    basic format finished for final project

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Talked with Prof. Magee and we did not see a purpose in having an __init__ or __repr__ method at the present. She told me to write this here
 #! Run this app with `python app.py` in your terminal and
 #! visit http://127.0.0.1:8050/ in your web browser.
+# much of my work is based off the Dash documentation: https://dash.plotly.com/interactive-graphing
 
 import dash
 import dash_core_components as dcc
@@ -12,6 +13,7 @@ import pandas as pd
 import os
 import matplotlib
 import numpy as np
+import plotly.graph_objects as go
 
 
 
@@ -23,7 +25,6 @@ class dataCleaning:
         df_list = []
         for file in files:
             if not file.startswith('.'):
-                print(file)
                 temp_df = pd.read_csv(folderpath+'/'+file)
                 df_list.append(temp_df)
         return df_list
@@ -33,7 +34,7 @@ class dataCleaning:
         for df in dfs:
             df.rename(columns=colNames, inplace=True)
 
-    #     print(df.columns)
+
     def combineVertically(dfs):
         """Takes list of dataframes and returns a single dataframe of dfs stacked vertically"""
         stack = pd.concat(dfs, axis=0)
@@ -42,17 +43,10 @@ class dataCleaning:
 
     def cleanedDailyAvg(dataFrame):
         """Takes a data frame. Averages variables by day"""
-        # temp = dataFrame[dataFrame['pH_total_scale'] != -999]
         temp = dataFrame
         temp.loc[temp['pH_total_scale']
                  == -999, ['pH_total_scale']] = np.nan
-        
-        # temp['Month'] = pd.DatetimeIndex(temp['Date']).month
-        # temp['Year'] = pd.DatetimeIndex(temp['Date']).year
-        # day = temp.resample('Y').mean()
-        # monthly = day.groupby(['Month'], ['Year']).mean()
         day = temp.groupby('Date').mean()
-      
         return  day
 
 
@@ -63,7 +57,7 @@ FL_dfs = dataCleaning.folderToDfs('analysis/data/FL')
 
 # Maine
 
-
+# renaming mess variable names 
 ME_cols =  {
     "pH_SW": "pH_total_scale",
     "pH_SW (Total Scale)": "pH_total_scale",
@@ -99,10 +93,7 @@ FL_cols = {
         'Licor Atm Pressure  (hPa)': 'Licor Atm Pressure (hPa)',
         'fCO2  Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
         'fCO2  SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
-        # 'fCO2 Air (sat) uatm': 'fCO2 Air (sat) (uatm)',
         'pCO2 Air (sat) uatm': 'pCO2 Air (sat) (uatm)',
-        # 'fCO2 SW (sat) uatm': 'fCO2 SW (sat) (uatm)',
-        # 'fCO2  SW (sat) (uatm)': 'fCO2 SW (sat) (uatm)',
         'pCO2 SW (sat) uatm': 'pCO2 SW (sat) (uatm)',
         "% O2": "MAPCO2 %O2",
         'DOXY': 'DOXY (umol/kg)',
@@ -126,8 +117,6 @@ GA_cols = {
     "pH (total scale)": "pH_total_scale",
     "pH SW": "pH_total_scale",
     "pH_Total_Scale": "pH_total_scale",
-    # 'fCO2 Air (sat) uatm': 'fCO2 Air (sat) (uatm)'
-
 }
 
 dataCleaning.renameCols(ME_dfs, ME_cols)
@@ -149,9 +138,9 @@ FL_DF_AVG = dataCleaning.cleanedDailyAvg(FL_DF)
 
 
 # add site variable for single data frame
-ME_DF_AVG['site'] = 'ME'
-GA_DF_AVG['site'] = 'GA'
-FL_DF_AVG['site'] = 'FL'
+ME_DF_AVG['site'] = 'Gulf of Maine'
+GA_DF_AVG['site'] = "Gray's Reef"
+FL_DF_AVG['site'] = 'Cheeca Rocks'
 
 # create one single data frame
 all_df_avg = pd.concat([ME_DF_AVG, GA_DF_AVG, FL_DF_AVG], axis=0)
@@ -159,10 +148,7 @@ all_df_avg = pd.concat([ME_DF_AVG, GA_DF_AVG, FL_DF_AVG], axis=0)
 
 # all_df_avg = all_df_avg[all_df_avg['xCO2 Air (wet) (umol/mol)'] > 300]
 
-#select varaibles to preserve
-# all_df_avg = all_df_avg[]
 
-# all_df_avg = all_df_avg[all_df_avg['CO2 SW QF'] == 2]
 
 # get list of variables for dropdown menu                         
 variables = all_df_avg.columns.sort_values()
@@ -176,13 +162,13 @@ variables_2_use = [
 # variable_dict = [{'var': 'DOXY (umol/kg)', 'desc': }]
 # get sites for site filter drop down
 sites = all_df_avg['site'].unique()
-sites
-print(variables.sort_values())
 
-# all_df_avg[all_df_avg[]]
-# temp = dataFrame[dataFrame['SST (C)'] > 0]
+#select varaibles to preserve
+# all_df_avg = all_df_avg[]
 
-##### filtering bad data???
+
+##### filtering bad data 
+
 all_df_avg.loc[all_df_avg['DOXY (umol/kg)'] < 0, ['DOXY (umol/kg)']] = np.nan
 all_df_avg.loc[all_df_avg['SST (C)'] < 0, ['SST (C)']] = np.nan
 all_df_avg.loc[all_df_avg['Salinity'] < 0, ['Salinity']] = np.nan
@@ -210,26 +196,45 @@ all_df_avg.loc[all_df_avg['xCO2 SW (wet) (umol/mol)']
                < 0, ['xCO2 SW (wet) (umol/mol)']] = np.nan
 
 
+### Creating map of study sites  #####
+siteLocals = {
+    'site': ['Gulf of Maine', "Gray's Reef",
+                      'Cheeca Rocks'],
+    'Latitude': [43.0200, 31.4020, 24.8977],
+    'Longitude': [-70.5400, - 80.8710, -80.6182]}
+sCords = pd.DataFrame(siteLocals, columns=['site', 'Latitude', "Longitude"])
+
+
+siteMap = px.scatter_mapbox(sCords, lat='Latitude', lon='Longitude', color='site')
+
+siteMap.update_layout(
+    mapbox_style="white-bg",
+    autosize=True,
+    mapbox=dict(center=dict(lat=34.5,lon=-75), zoom=2.8),
+    mapbox_layers=[
+        {
+            "below": 'traces',
+            "sourcetype": "raster",
+            "sourceattribution": "United States Geological Survey",
+            "source": [
+                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
+            ]
+        }
+    ],
+    
+    )
+
+
+# fig = px.scatter_mapbox()
+
 
 ################################# dash app #######################
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# using some of the default plotly css styles
+external_stylesheets = [ 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
-# print(ME_DF_AVG['site'])
-
-graph_me = px.scatter(all_df_avg, y="pH_total_scale",
-                      trendline="ols", color='site')
-graph_2 = px.scatter(all_df_avg[all_df_avg['SST (C)'] > 0], y="pH_total_scale", x="SST (C)",
-                     trendline="ols", color='site')
-graph_fl = px.scatter(FL_DF_AVG, y="pH_total_scale", trendline="ols")
-
-
-# 3 input parameters I think (1) Y variable (2) x variable (3)site (4??) avg by day, month, year?
 
 # Getting date to be apart of the xvariables
 xVariables = [{'label': i, 'value': i}
@@ -238,22 +243,30 @@ dateLabel = {'label': "Date", 'value': ''}
 xVariables.append(dateLabel)
 
 
+# structuring the webpage 
 app.layout = html.Div(children=[
 
-    html.H1(children='Ocean Acidification'),
-    html.P(children=f"Number of varibles: {len(variables_2_use)}. DataFrame dimensions: {all_df_avg.shape}"),
+    html.H1(children='Interactive Ocean Acidification Data Explorer'),
 
     html.Div(id = "interactive plot",children =[ 
-        dcc.Dropdown(
+        html.Div(id='variblesContainer', children=[
+            html.Div(children=[
+                html.P(className="axisLabel", children=['X axis']),
+                dcc.Dropdown(
                 id='xaxis',
-            options=xVariables,
+                options=xVariables,
                 value=''
-                ),
-        dcc.Dropdown(
-            id='yaxis',
-            options=[{'label': i, 'value': i} for i in variables_2_use],
-            value="pH_total_scale"
-        ),
+            ), ]),
+            html.Div(children=[
+                html.P(className="axisLabel",children=["Y axis"]),
+                dcc.Dropdown(
+                id='yaxis',
+                options=[{'label': i, 'value': i} for i in variables_2_use],
+                value="pH_total_scale"
+            ), ]),
+          
+        ]),
+        
         
         dcc.Dropdown(
             id='site_selector',
@@ -264,38 +277,37 @@ app.layout = html.Div(children=[
 
 
         ),
-        dcc.RadioItems(
-            id='avgRange',
-            options=[
-                {'label': 'Daily', 'value': 'D'},
-                {'label': 'Weekly', 'value': 'W'},
-                {'label': 'Monthly', 'value': 'M'},
-                {'label': 'Yearly', 'value': 'Y'},
-            ],
-            value= 'Y'
-        ),
+       
         dcc.Graph(id='indicator-graphic'),
+        html.H2(children='Study sites'),
+        html.Div(id='mapDiv',children=[
+            dcc.Graph(id='map', figure=siteMap),
+            html.Div(id='siteDescrip',children=[
+                html.P(children="For more information on Ocean Acidification check out"),
+                html.A(
+                    children="https://oceanacidification.noaa.gov/OurChangingOcean.aspx", href='https://oceanacidification.noaa.gov/OurChangingOcean.aspx'),
+            ])
+        ]),
+        
    ]),
+   
    html.Div(id='citation',
-            children="Data from Sutton, A. J., Feely, R. A., Maenner-Jones, S., Musielwicz, S., Osborne, J., Dietrich, C., Monacci, N., Cross, J., Bott, R., Kozyr, A., Andersson, A. J., Bates, N. R., Cai, W.-J., Cronin, M. F., De Carlo, E. H., Hales, B., Howden, S. D., Lee, C. M., Manzello, D. P., McPhaden, M. J., Meléndez, M., Mickett, J. B., Newton, J. A., Noakes, S. E., Noh, J. H., Olafsdottir, S. R., Salisbury, J. E., Send, U., Trull, T. W., Vandemark, D. C., and Weller, R. A. (2019): Autonomous seawater pCO2 and pH time series from 40 surface buoys and the emergence of anthropogenic trends, Earth Syst. Sci. Data, 11, 421-439, https://doi.org/10.5194/essd-11-421-2019.")
+            children=[
+                html.H3(children='Data Source'),
+                html.Em(id='citeText',
+                       children=" Sutton, A. J., Feely, R. A., Maenner-Jones, S., Musielwicz, S., Osborne, J., Dietrich, C., Monacci, N., Cross, J., Bott, R., Kozyr, A., Andersson, A. J., Bates, N. R., Cai, W.-J., Cronin, M. F., De Carlo, E. H., Hales, B., Howden, S. D., Lee, C. M., Manzello, D. P., McPhaden, M. J., Meléndez, M., Mickett, J. B., Newton, J. A., Noakes, S. E., Noh, J. H., Olafsdottir, S. R., Salisbury, J. E., Send, U., Trull, T. W., Vandemark, D. C., and Weller, R. A. (2019): Autonomous seawater pCO2 and pH time series from 40 surface buoys and the emergence of anthropogenic trends, Earth Syst. Sci. Data, 11, 421-439, https://doi.org/10.5194/essd-11-421-2019.")
+            ])
 
 ])
 
-
+# making figure interactive
 @app.callback(
     Output('indicator-graphic', 'figure'),
     Input('xaxis', 'value'),
     Input('yaxis', 'value'),
     Input('site_selector', "value"),
-    Input('avgRange', 'value')
 )
-def update_graph(xaxis, yaxis, site_selector, avgRange):
-    
-   
-
-
-    print(f"site_selector {site_selector}")
-    print(type(site_selector))
+def update_graph(xaxis, yaxis, site_selector, ):
 
     if type(site_selector) == list:
         data = all_df_avg[all_df_avg.site.isin(
@@ -303,8 +315,6 @@ def update_graph(xaxis, yaxis, site_selector, avgRange):
     else:
         data = all_df_avg[all_df_avg['site'] == site_selector]
 
-   
-    
 
     if xaxis == '':
         fig = px.scatter(data, y=data[yaxis], trendline="ols", color='site')
